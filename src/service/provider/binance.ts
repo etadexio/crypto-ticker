80053b829656298afaee0386831cb66748409dd5
hfa0 <fakhir.husein@gmail.com>
2020-05-18 14:42:35 +0200

    add service

---
+++ src/service/provider/binance.ts
@@ -0,0 +1,73 @@
+import { Pair, Exchange, IPriceTicker, ExchangeService } from '../types';
+import {
+  getPairSymbol,
+  getEventName,
+  getEventNameOpposite,
+} from '../utils/mapping';
+import { EventEmitterInstance } from '../event-emitter';
+
+export class BinanceService implements ExchangeService {
+  private socket = new WebSocket('wss://stream.binance.com:9443/ws');
+  private ids: Pair[] = [];
+  constructor() {
+    this.socket.onmessage = (message: any) => {
+      const data = JSON.parse(message);
+      const myPair = getEventNameOpposite(
+        data.s.toLowerCase(),
+        Exchange.BINANCE
+      );
+      if (!myPair) {
+        throw new Error(`invalid pair ${myPair}, ${data.s}`);
+      }
+      const { c, o, l, h, v, E, p, P } = data;
+      const event: IPriceTicker = {
+        e: Exchange.BINANCE,
+        s: myPair,
+        c,
+        o,
+        l,
+        h,
+        v,
+        lastc: E,
+        p,
+        P,
+      };
+      EventEmitterInstance.emit(getEventName(myPair, Exchange.BINANCE), event);
+    };
+  }
+  public subscribe = (pair: Pair) => {
+    if (this.ids.find(id => id === pair)) {
+      console.log('socket already subscribed', Exchange.BINANCE);
+      return;
+    }
+    const data = {
+      method: 'SUBSCRIBE',
+      params: [`${getPairSymbol(pair, Exchange.BINANCE)}@ticker`],
+      id: pair,
+    };
+    this.send(data) && this.ids.push(pair);
+  };
+
+  public unsubscribe = (pair: Pair) => {
+    const index = this.ids.indexOf(pair);
+    const data = {
+      method: 'UNSUBSCRIBE',
+      params: [`${getPairSymbol(pair, Exchange.BINANCE)}@ticker`],
+      id: pair,
+    };
+    this.send(data) && this.ids.splice(index, 1);
+  };
+
+  private send(data: any): boolean {
+    if (!this.socket.OPEN) {
+      console.log('socket not ready', Exchange.BINANCE);
+      return false;
+    }
+    this.socket.send(JSON.stringify(data));
+    return true;
+  }
+
+  close() {
+    this.socket.close();
+  }
+}
---
+++ src/service/types/index.ts
@@ -0,0 +1,32 @@
+export enum Pair {
+  BTC_USDT = 'BTC_USDT',
+}
+
+export enum Exchange {
+  BINANCE = 'BINANCE',
+}
+
+export const pairMap = {
+  [Exchange.BINANCE]: {
+    [Pair.BTC_USDT]: 'btcusdt',
+  },
+};
+
+export interface IPriceTicker {
+  e: Exchange;
+  s: Pair;
+  c: number;
+  o: number;
+  l: number;
+  h: number;
+  v: number;
+  lastc: number;
+  p: number;
+  P: number;
+}
+
+export interface ExchangeService {
+  subscribe: (pair: Pair) => void;
+  unsubscribe: (...args: any[]) => void;
+  close: () => void;
+}
